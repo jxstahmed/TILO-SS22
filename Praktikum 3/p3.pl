00834302280613484d4602ddb749069791bd3536
Ahmed J <103430690+jxstahmed@users.noreply.github.com>
2022-05-06 12:00:50 +0200

    Final Version

--- Praktikum 3/p3.pl
+++ Praktikum 3/p3.pl
@@ -1,41 +1,45 @@
 % Aufgabe 1
 
 app(nil,Xs,Xs).
-app(list(X,X1s),Ys,list(X,X2s)):- app(X1s,Ys,X2s).
+app(list(X,Xs),Ys,list(X,As)):- app(Xs,Ys,As).
 
-% linListe(list(A,Ys)) : list(A,Ys) ist eine gültige Liste, wenn Ys eine gültige Liste ist.
+% linListe(Xs): Xs ist eine gueltige Liste.
 linListe(nil).
-linListe(list(A,Ys)) :- linListe(Ys).
+linListe(list(X,Ys)) :- linListe(Ys), not(linListe(X)).
 
-% member2(X,list(X,Xs)). : Das Element X befindet sich in der liste.
+% member2(X,Ys): Ys enthält das Element X
 member2(X,list(X,Xs)).
 member2(X,list(Y,Ys)). :- member2(X,Ys).
 
-% infix(Xs,Ys) : Die Liste X2 ist in der Liste Ys vorhanden.
-infix(Xs,Ys) :- app(A,Xs,Bs), app(Bs,A,Ys).
+% infix(Xs,Ys) : Die Liste Xs ist in der Liste Ys vorhanden.
+infix(Xs,Ys) :- app(_,Xs,As), app(As,_,Ys), linListe(As), linListe(Xs), linListe(Ys).
 
 % attach(Xs,X,Ys) : Die Liste Ys ist die Liste Xs erweitert um Element X.
-attach(Xs,X,Ys) :- app(Xs,X,Ys).
+attach(Xs,nil,Xs).
+attach(Xs,X,Ys) :- app(Xs,list(X,nil),Ys), linListe(Ys), linListe(Xs).
 
 % rev(Xs,Ys) : Ys ist die gespiegelte Liste Xs.
 rev(nil,nil).
-rev(list(X,Xs),Ys) :- rev(Xs,Ys), attach(Ys,X,Xs).
+rev(list(X,Xs),Ys) :- rev(Xs,As), attach(As,X,Ys), linListe(Xs), linListe(Ys).
 
 
 % Aufgabe 2
 
-% binbaum(Root,Lb,Rb,node(Root,Lb,Rb)) : Node besteht aus der Beschriftung Root, dem linken Teilbaum Lb und dem Rechten Teilbaum Rb.
-binbaum(leer).
-binbaum(node(eintrag,links,rechts)) :- binbaum(links), binbaum(rechts).
-binbaum(Root,Lb,Rb,node(Root,Lb,Rb)) :- binbaum(Lb), binbaum(Rb).
+% binbaum(X) : X ist ein binärer Baum.
+binbaum(empty).
+binbaum(node(Eintrag,Lb,Rb)) :- binbaum(Lb), binbaum(Rb).
 
-% natSymb(o) :- o ist die 0 in Darstellung der Natürlichen Symbole.
+% construct(Root,Lb,Rb,Xneub): Xneub ist der Baum mit der Wurzel Eintrag, linkem Teilbaum Lb und rechtem Teilbaum Rb.
+construct(Eintrag,Lb,Rb,node(Root,Lb,Rb)) :- binbaum(Lb), binbaum(Rb).
+
+% natSymb(X) :- X ist eine natürliche Zahl.
 natSymb(o).
 natSymb(s(X)):- natSymb(X).
-% add(X,Y,A) : A ist das Ergebnis bei Addition von X und Y.
+
+% add(X,Y,A) : A ist das Ergebnis bei einer Addition von X und Y.
 add(X,o,X):- natSymb(X).
-add(X,s(Y),s(A)):- add(X,Y,A).
+add(X,s(Y),s(A)):- add(X,Y,A), natSymb(X), natSymb(Y), natSymb(A).
 
 % knotenanz(Xb,N) :- N ist die Anzahl der Knoten des Baumes Xb.
-knotenanz(nil,o).
-knotenanz(node(Eintrag,Links,Rechts),s(N)) :- knotenanz(Links, N1), knotenanz(Rechts, N2), add(N1,N2,N).
+knotenanz(empty,o).
+knotenanz(node(Eintrag,Lb,Rb),s(N)) :- knotenanz(Lb,N1), knotenanz(Rb,N2), add(N1,N2,N), binbaum(Lb), binbaum(Rb), natSymb(N1), natSymb(N2), natSymb(N).
